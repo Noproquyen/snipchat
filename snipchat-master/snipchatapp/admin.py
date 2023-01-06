45dc328e07ad780b848e08e99d4c149fed5dd53b
Noproquyen <120568811+Noproquyen@users.noreply.github.com>
2023-01-06 14:30:54 +0700

    Delete snipchat-master directory

--- snipchat-master/snipchatapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from snipchatapp.models import Snippets, Comments
-from django.contrib.auth.models import User
-
-admin.site.register(Snippets)
-admin.site.register(Comments)
